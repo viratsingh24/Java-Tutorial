f69f746c888d4390dca0bb86d355f0482f8ff2e8
viratsingh24 <114825043+viratsingh24@users.noreply.github.com>
2024-01-12 23:32:21 +0530

    Add files via upload

---
+++ Myfirst_1.java
@@ -0,0 +1,17 @@
+class virat{
+    public static void main(String args[]){
+        //output
+        //print is a function
+        //println is used to break the line
+        System.out.println("hello virat singh");
+        System.out.println("hello virat singh");
+        //\nis also used to break the line 
+        System.out.print("hello virat singh\n");
+        System.out.print("hello virat singh\n");
+        //learn all the things related to 
+        //print
+        //println
+        System.out.print("virat\nsingh\ntomar");
+        //\n
+    }
+}
